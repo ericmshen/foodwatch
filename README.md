f6a3bc27523fd404d0b0a0c4c82646663df1a080
OustanDing <oustan10@gmail.com>
2018-09-07 20:27:06 -0400

    fixed stuff

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # foodmanager
-<<<<<<< HEAD
 oustan bmi = 0
 oustan bmi -= 1
 
-=======
 oustan bmi = 2
 eric bmi = 500
->>>>>>> bdf245f94f10ad9fd9560491e2a2e1607637521f
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import *
+
+app = Flask(__name__)
+
+app.secret_key = 'ericbmi500'
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SESSION_FILE_DIR'] = mkdtemp()
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+
+if __name__ == '__main__:
+    app.run(debug=True)
