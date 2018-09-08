334415ce8a7baad51e0eccaa638ffb09622dcdd5
OustanDing <oustan10@gmail.com>
2018-09-08 01:32:57 -0400

    test send sms

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import db, credentials
 from flask import *
 from tempfile import mkdtemp
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -12,12 +13,18 @@ app.config['SESSION_FILE_DIR'] = mkdtemp()
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
+
+# Firebase Database
 cred = credentials.Certificate('foodlord-5dd61-firebase-adminsdk-2ksfc-9d4371b135.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://foodlord-5dd61.firebaseio.com/'
     })
 
-# root = db.reference()
+# Twilio
+account_sid = 'ACa209e1ae289d60729d7321952c4d8974'
+auth_token = '483d41d0e680edbfa018d0f0cfc6c578'
+
+client = Client(account_sid, auth_token)
 
 @app.route('/')
 def index():
@@ -28,6 +35,11 @@ def index():
             'name': foodData['name'],
             'expiry': foodData['expiry']
             })
+    message = client.messages.create(
+        to="+14165539697",
+        from_="+12672146320",
+        body="u have the brain of a neanderthal")
+    print(message.sid)
     return render_template('index.html', foods = foods)
 
 if __name__ == '__main__':
