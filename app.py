1aa04582b595df614babf58bcc6e01692ade0a6e
OustanDing <oustan10@gmail.com>
2018-09-08 00:10:56 -0400

    connected database

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import firebase_admin
+from firebase_admin import db, credentials
 from flask import *
 from tempfile import mkdtemp
 
@@ -10,9 +12,21 @@ app.config['SESSION_FILE_DIR'] = mkdtemp()
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
+cred = credentials.Certificate('foodlord-5dd61-firebase-adminsdk-2ksfc-9d4371b135.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://foodlord-5dd61.firebaseio.com/'
+    })
+
+root = db.reference()
+
 @app.route('/')
 def index():
-    return 'Hello world!'
+    new_food = root.child('items').push({
+        'name': 'Banana',
+        'expiry': '09/10/2018'
+        })
+    banana = db.reference('items/{0}'.format(new_food.key)).get()
+    return render_template('index.html', banana = banana)
 
 if __name__ == '__main__':
     app.run(debug=True)
