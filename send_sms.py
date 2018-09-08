9211407784d984941bf785f913cab921d4e68268
Derek Yang <derekyang@huntsman-ve702-4854.apng.wlan.private.upenn.edu>
2018-09-08 01:45:02 -0400

    sms.py

---
+++ send_sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = 'ACa209e1ae289d60729d7321952c4d8974'
+auth_token = '483d41d0e680edbfa018d0f0cfc6c578'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              from_='+12672146320',
+                              body='hi this is a test',
+                              to='+14165539697'
+                          )
+
+print(message.sid)uio 
